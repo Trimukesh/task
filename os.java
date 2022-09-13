9fc56fafd833ea8ab40c3bc54b309ee3cbb1ad72
Trimukesh <109743652+Trimukesh@users.noreply.github.com>
2022-09-13 15:04:57 +0530

    Create os.java

---
+++ os.java
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+src = 'C:\Windows\System32\actions-runner\_work\task\task'
+dest = 'C:\Windows\System32\actions-runner'
+
+#src_files = os.listdir(src)
+
+for root, dirs, files in os.walk(src):  
+    for filename in files:
+        full_file_name = os.path.join(src, filename)
+        if (os.path.isfile(full_file_name)):
+            if full_file_name.endswith('.yaml'):
+                shutil.copy(full_file_name, dest)
